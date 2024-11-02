9feaaf14483692d333a247de0cf2c6ff28a01775
Madhu Mohan Reddy <128889553+ymadhumohanreddy@users.noreply.github.com>
2024-11-02 08:05:55 +0530

    fifth commit

--- app/page.js
+++ app/page.js
@@ -11,7 +11,7 @@ export default function Home() {
       <section className=" z-50">
         <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
           <a href="#" className="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700" role="alert">
-            <span className="text-xs bg-primary rounded-full text-white px-4 py-1.5 mr-3">Hint</span> <span className="text-sm font-medium">Scroll Down</span> 
+            <span className="text-xs bg-primary rounded-full text-white px-4 py-1.5 mr-3">Tip?</span> <span className="text-sm font-medium">Scroll Down</span> 
             <svg className="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd"></path></svg>
         </a>
           <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white font-serif">Your Personal AI Interview Coach</h1>
@@ -25,7 +25,7 @@ export default function Home() {
             <a href="https://www.youtube.com/watch?v=IdTMDpizis8" target="_blank" 
   rel="noopener noreferrer"  className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
               <svg className="mr-2 -ml-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path></svg>
-              Need confidence?
+              Need Motivation ?
               
             </a>  
           </div>
