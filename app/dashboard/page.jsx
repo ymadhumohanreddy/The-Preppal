99c81f5e533951d4a2ec7cd03bb83e752dc65d04
Madhu Mohan Reddy <128889553+ymadhumohanreddy@users.noreply.github.com>
2024-11-05 17:27:06 +0530

    dashboard commit

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -5,8 +5,8 @@ import InterviewList from './_components/InterviewList'
 function Dashborad() {
   return (
     <div className='p-10'>
-      <h2 className='font-serif  text-2xl'>Dashboard</h2>
-      <h2 className='text-gray-500 font-serif'>Create and Start your AI Mockup Interview</h2>
+      <h2 className='font-serif  text-2xl'>Create  your Interview</h2>
+      <h2 className='text-gray-500 font-serif'>Try to answer atleast one question to receive feedback.</h2>
       <div className='grid grid-cols-1 md:grid-cols-3 my-5'>
         <AddNewInterview/>
       </div>
