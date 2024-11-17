0d80cd46f095dd00051ed5974fd10235c76cb824
Madhu Mohan Reddy <128889553+ymadhumohanreddy@users.noreply.github.com>
2024-11-17 10:40:54 +0530

    changes

--- app/aboutme/page.jsx
+++ app/aboutme/page.jsx
@@ -1,17 +1,30 @@
-import React from 'react';
+"use client"
+import React, { useState } from 'react';
 import { FaGithub, FaInstagram, FaLinkedin } from 'react-icons/fa';
-import Header from '../dashboard/_components/Header'; // Make sure to import your Header component
 import Spline from '@splinetool/react-spline';
 
 function AboutMe() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleLoad = () => setIsLoading(false);
+
   return (
     <>
       <section className="flex flex-col items-center justify-center h-[calc(90vh)] overflow-hidden">
         
-        <Spline 
-          scene="https://prod.spline.design/UlThdMSAAnJw9kvW/scene.splinecode" 
-          aria-label="3D interactive scene showing personal work" 
+        {/* 3D Spline Scene */}
+        {isLoading && (
+          <div className="absolute z-10 flex items-center justify-center w-full h-full bg-white">
+            <div className="loader border-4 border-blue-400 border-t-transparent rounded-full w-12 h-12 animate-spin"></div>
+          </div>
+        )}
+        <Spline
+          scene="https://prod.spline.design/UlThdMSAAnJw9kvW/scene.splinecode"
+          onLoad={handleLoad}
+          aria-label="3D interactive scene showing personal work"
         />
+
+        {/* Message Section */}
         <br />
         <h2 className="text-xl font-serif mb-4 text-center">
           "Hope we all get placed! All I can say is, <strong>Good luck</strong>, and <strong>Have a Great Day!</strong> ;)"
@@ -25,6 +38,7 @@ function AboutMe() {
           </a>
         </h2>
 
+        {/* Social Media Links */}
         <div className="flex space-x-4">
           <a 
             href="https://www.linkedin.com/in/madhu-yeddula/" 
@@ -51,6 +65,7 @@ function AboutMe() {
             <FaInstagram className="text-blue-400 text-3xl transition hover:text-pink-500" />
           </a>
         </div>
+        
       </section>
     </>
   );
