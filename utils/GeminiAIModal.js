26a93cb5e843d7e0a63507120e4297119fcfb5bd
Madhu Mohan Reddy <128889553+ymadhumohanreddy@users.noreply.github.com>
2024-11-20 19:04:42 +0530

    new api key

--- utils/GeminiAIModal.js
+++ utils/GeminiAIModal.js
@@ -4,21 +4,20 @@ const {
   HarmBlockThreshold,
 } = require("@google/generative-ai");
 
-const apiKey = process.env.NEXT_PUBLIC_GEMINI_API_KEY;
+const apiKey = process.env.GEMINI_API_KEY;
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
-  model: "gemini-1.5-flash",
+  model: "gemini-1.5-flash-8b",
 });
 
 const generationConfig = {
   temperature: 1,
   topP: 0.95,
-  topK: 64,
+  topK: 40,
   maxOutputTokens: 8192,
   responseMimeType: "text/plain",
 };
-
 
 
 export  const chatSession = model.startChat({
