7ab04075cfb8d187bbf538a85e8d823a9e25feed
Madhu Mohan Reddy <128889553+ymadhumohanreddy@users.noreply.github.com>
2025-02-12 10:57:22 +0530

    analy

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "sonner";
 import Header from "./dashboard/_components/Header";
+import { Analytics } from '@vercel/analytics/next';
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -33,6 +34,7 @@ export default function RootLayout({ children }) {
           <Header />
           
           {children}
+          <Analytics />
         </body>
       </html>
     </ClerkProvider>
