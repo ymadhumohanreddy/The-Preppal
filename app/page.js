12852f7b664ebd009e14d6268077ea576bddfc92
ymadhumohanreddy <yeddulamadhu6@gmail.com>
2025-06-23 22:11:03 +0530

    youtube video

--- app/page.js
+++ app/page.js
@@ -158,7 +158,7 @@ const benefits = [
             </a>
 
             <a
-              href="https://www.youtube.com/watch?v=2vU5MoO1ZSM"
+              href="https://youtu.be/Wqp6T7NhU5E"
               target="_blank"
               rel="noopener noreferrer"
               className="font-serif inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
@@ -171,7 +171,7 @@ const benefits = [
               >
                 <path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path>
               </svg>
-              Need Motivation?
+              Watch Demo
             </a>
           </div>
         </div>
